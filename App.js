450782e4f29ef2a5306045c67f9a1e8026ee7cce
Stephen Banks <stephen.banks@airelogic.com>
2024-05-01 11:07:48 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, Image, Button, View } from 'react-native';
+import MealDbRoutes from './api/mealDbRoutes';
 
 export default function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  const getRandomRecipe = async () => {
+    const url = MealDbRoutes.generateRandomRecipe;
+    const response = await fetch(url);
+    const json = await response.json();
+    setRecipes(json.meals);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      { <FlatList
+        data={recipes}
+        style={styles.recipe}
+        keyExtractor={({idMeal}) => idMeal}
+        renderItem={({item}) => (
+          <View>
+            <Image style={styles.tinyLogo} source={{uri: item.strMealThumb}} />
+            <Text>{item.strMeal} - {item.strCategory}</Text>
+          </View>
+        )}
+      />}
+      <Button title="Press me" style={styles.button} onPress={getRandomRecipe}></Button>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: "#fff",
+    padding: 20
   },
+  tinyLogo: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center'
+  },
+  button: {
+    flex: 3,
+    alignSelf: 'center'
+  },
+  recipe: {
+    flex: 2,
+    alignSelf: 'center',
+    marginVertical: 50
+  }
 });
---
+++ api/mealDbRoutes.js
@@ -0,0 +1,29 @@
+const baseUrl = "http://www.themealdb.com/api/json/v1/1";
+
+export default class MealDbRoutes {
+  static generateRandomRecipe = baseUrl + "/random.php";
+  static recipeCategories = baseUrl + "/categories.php";
+  static categoryList = baseUrl + "/list.php?c=list";
+  static areaList = baseUrl + "/list.php?a=list";
+  static ingredientList = baseUrl + "/list.php?i=list";
+
+  static getReicpeById(id) {
+    return baseUrl + "/lookup.php?i=" + id;
+  }
+
+  static listAll(type) {
+    return baseUrl + "/list.php?" + type + "=list";
+  }
+
+  static getByIngredient(ingredient) {
+    return baseUrl + "/filter.php?i=" + ingredient;
+  }
+
+  static getByCategory(category) {
+    return baseUrl + "/filter.php?c=" + category;
+  }
+
+  static getByArea(area) {
+    return baseUrl + "/filter.php?a=" + area;
+  }
+}
